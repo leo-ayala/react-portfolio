7da6182c3b33bbd24c9293afb0be828b9324160b
Leo Ayala <leo_a1016@yahoo.com>
2020-10-31 20:36:12 -0400

    Updates

---
+++ precache-manifest.31885d4265c4a0fc0420e4602395c0f4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e8f4f6b93dedfe777862ebc5a25581",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3cd927000dbc26f239e8",
+    "url": "/react-portfolio/static/css/2.56e46b0c.chunk.css"
+  },
+  {
+    "revision": "42216591d846b63d844e",
+    "url": "/react-portfolio/static/css/main.92f363af.chunk.css"
+  },
+  {
+    "revision": "3cd927000dbc26f239e8",
+    "url": "/react-portfolio/static/js/2.71eec8b4.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react-portfolio/static/js/2.71eec8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42216591d846b63d844e",
+    "url": "/react-portfolio/static/js/main.15b080bf.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a9db0573b9f8ccc2cda8c6596dae29b2",
+    "url": "/react-portfolio/static/media/leoayala_resume.a9db0573.pdf"
+  }
+]);
